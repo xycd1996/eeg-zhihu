49d3da26d470c3fc19e780efc4ec4251626b8cb7
xyc960117 <515838785@qq.com>
2020-07-20 10:26:22 +0800

    push

--- config/config.default.ts
+++ config/config.default.ts
@@ -18,7 +18,7 @@ export default (appInfo: EggAppInfo) => {
 
   config.sequelize = {
     dialect: 'mysql',
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 3306,
     database: 'zhi_hu',
     username: 'root',
